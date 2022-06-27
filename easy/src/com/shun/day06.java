6e3aed96c2054141448888bec6bf9e62dedcf74a
chushunshun <1173023051@qq.com>
2022-06-28 01:00:25 +0800

    feat(第六天): 合并排序链表

--- easy/src/com/shun/day06.java
+++ easy/src/com/shun/day06.java
@@ -7,16 +7,16 @@ import java.util.Map;
 import java.util.Stack;
 
 /**
- * @Description: 有效的括号(https://leetcode.cn/problems/valid-parentheses/)
+ * @Description: 有效的括号(https://leetcode.cn/problems/merge-two-sorted-lists/submissions/)
  * @author: chushunshun
  * @date: 2022/6/27 10:12
  * @param:
  */
 public class day06 {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(1,new ListNode(6,null));
-        ListNode list2 = new ListNode(1,new ListNode(2,new ListNode(4,null)));
-        System.out.println(mergeTwoLists(list1,list2));
+        ListNode list1 = new ListNode(1,new ListNode(2,new ListNode(4,null)));
+        ListNode list2 = new ListNode(1,new ListNode(3,new ListNode(4,null)));
+        System.out.println(mergeTwoLists2(list1,list2));
 
     }
 
@@ -30,32 +30,62 @@ public class day06 {
         }
 
         ListNode next = list1;
-        ListNode first = list2;
+        ListNode compare = list2;
 
         ListNode nextLastNode = null;
-        while (next != null && first != null){
-            ListNode listNode;
-            if (next.val >= first.val){
-                nextLastNode = next;
-                if (null == first.next){
-                    first.next = next;
+
+        while (next != null && compare != null){
+            if (next.val >= compare.val){
+                nextLastNode = compare;
+                if (null == compare.next){
+                    compare.next = next;
                     next = null;
                 }
-                first = first.next;
+                compare = compare.next;
+
             }else {
-                //first = first.next;
+                ListNode listNode = next.next;
                 if (nextLastNode != null){
+                    next.next = nextLastNode.next;
                     nextLastNode.next = next;
-                    next.next = first;
-
-                    nextLastNode.next = next.next;
+                }else {
+                    next.next = compare;
                 }
-                next = next.next;
-            }
 
+                nextLastNode = next;
+                next = listNode;
+            }
         }
         return list2.val > list1.val ? list1 : list2;
     }
+
+
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        // 类似归并排序中的合并过程
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
+    }
+
+
+
 
     static class ListNode {
         int val;
