7b0af809b020889f41f5bd70e91f0dcd80f51d67
chushunshun <1173023051@qq.com>
2022-06-28 15:19:18 +0800

    feat(第七天): 删除所有数组中的重复项

---
+++ easy/src/com/shun/day07.java
@@ -0,0 +1,30 @@
+package com.shun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 删除有序数组中的重复项(https://leetcode.cn/problems/remove-duplicates-from-sorted-array/)
+ * @author: chushunshun
+ * @date: 2022/6/27 10:12
+ * @param:
+ */
+public class day07 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,2,2,2,5,4};
+        System.out.println(removeDuplicates(nums));
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 0;
+        int i = 1;
+        while (i < nums.length) {
+            if (nums[j] != nums[i]){
+                nums[++j] = nums[i];
+            }
+            i++;
+        }
+        return ++j;
+    }
+}
